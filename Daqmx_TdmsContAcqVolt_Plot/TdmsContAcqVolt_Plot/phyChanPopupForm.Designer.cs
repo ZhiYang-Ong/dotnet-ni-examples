f4f2037256db0b4ec98bdb5cb6e390b5a3e0d4ed
ZY-Ong <ongzy93@gmail.com>
2023-01-25 21:27:20 -0500

    Upload working example

---
+++ Daqmx_TdmsContAcqVolt_Plot/TdmsContAcqVolt_Plot/phyChanPopupForm.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace PhyChanPopup
+{
+    partial class phyChanPopupForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(phyChanPopupForm));
+            this.phyChanListBox = new System.Windows.Forms.ListBox();
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // phyChanListBox
+            // 
+            this.phyChanListBox.FormattingEnabled = true;
+            this.phyChanListBox.ItemHeight = 20;
+            this.phyChanListBox.Location = new System.Drawing.Point(28, 26);
+            this.phyChanListBox.Name = "phyChanListBox";
+            this.phyChanListBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.phyChanListBox.Size = new System.Drawing.Size(331, 344);
+            this.phyChanListBox.TabIndex = 0;
+            // 
+            // okButton
+            // 
+            this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.okButton.Location = new System.Drawing.Point(63, 389);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(102, 31);
+            this.okButton.TabIndex = 1;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(222, 389);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(102, 31);
+            this.cancelButton.TabIndex = 1;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // phyChanPopupForm
+            // 
+            this.AcceptButton = this.okButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancelButton;
+            this.ClientSize = new System.Drawing.Size(392, 450);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.phyChanListBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "phyChanPopupForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Select Item(s)";
+            this.Load += new System.EventHandler(this.phyChanPopupForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox phyChanListBox;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+    }
+}
+
---
+++ Daqmx_TdmsContAcqVolt_Plot/TdmsContAcqVolt_Plot/phyChanPopupForm.cs
@@ -0,0 +1,81 @@
+﻿using NationalInstruments.DAQmx;
+using NationalInstruments.Restricted;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Windows.Forms;
+using static System.Windows.Forms.ListBox;
+
+namespace PhyChanPopup
+{
+    public partial class phyChanPopupForm : Form
+    {
+        public phyChanPopupForm()
+        {
+            InitializeComponent();
+        }
+
+        public string phyChannels
+        {
+            get { return phyChanSyntax; }
+        }
+
+        private string phyChanSyntax = "";
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            const string chanSearchKey = "/ai";
+            bool first = true;
+            int i = 1;
+            string thisItem, tempItem = "", lastItem = "";
+            foreach (var item in phyChanListBox.SelectedItems)
+            {
+                thisItem = item.ToString();
+                if (first == true)
+                {
+                    phyChanSyntax = thisItem;
+                    lastItem = thisItem;
+                    first = false;
+                }
+                else
+                {
+                    // If the current device is same as previous one, concatenate the channels
+                    if (thisItem.Substring(0, thisItem.IndexOf(chanSearchKey)) == lastItem.Substring(0, lastItem.IndexOf(chanSearchKey)))
+                    {
+                        tempItem = lastItem;
+                        phyChanSyntax = phyChanSyntax.Substring(0, phyChanSyntax.Length - lastItem.Length - 2);
+                        if (tempItem.Contains(":"))
+                        {
+                            tempItem = tempItem.Substring(0, tempItem.IndexOf(":") + 1)
+                                + thisItem.Substring(thisItem.IndexOf(chanSearchKey) + 3);
+                        }
+                        else
+                        {
+                            tempItem = lastItem + ":" + thisItem.Substring(thisItem.IndexOf(chanSearchKey) + 3);
+                        }
+                        phyChanSyntax += tempItem;
+                    }
+                    else
+                        phyChanSyntax += thisItem;
+                    lastItem = tempItem;
+                }
+
+                if (i != phyChanListBox.SelectedItems.Count)
+                    phyChanSyntax += ", ";
+                i++;
+            }
+            Close();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void phyChanPopupForm_Load(object sender, EventArgs e)
+        {
+            phyChanListBox.Items.Clear();
+            phyChanListBox.Items.AddRange(DaqSystem.Local.GetPhysicalChannels(PhysicalChannelTypes.AI, PhysicalChannelAccess.External));
+        }
+    }
+}
